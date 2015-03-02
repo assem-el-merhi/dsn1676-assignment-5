b3a2617cb570a5e47ff421a159ab5023a30982c4
assem-el-merhi <Assem@Assems-MacBook-Pro.local>
2015-03-02 16:22:06 -0500

    Retyped everything
    Added to to list we weren’t practising anymore so named to do list for
    the i changed new-input to written because I found it was to
    complicated as the nam is input.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
-var $form = $('form');
-var $add = $('#add');
+var $form = $('.form');
+var $written = $('.written');
+var $list = $('.list');
 
-
-$form.on('submit', function (e) {
-    var $li = $('<li>');
-    $li.addClass('new-input');
-    $('body').append($li);
+$form.on('submit', function(e) {
     e.preventDefault();
-    //$circle.css('background-color', $theColor.val());
-
+    //Creating the list
+    	
+    //by creating a button for the list this will allow mutiple buttons
+    var $li = $('<li>').html($written.val());
+    var $writtenDl = $('<button class="close">X</button>');
+    	
+    //adding button for event 
+    $writtenDl.on('click', function () {
+    $li.remove('li');
+    
+    });
+    
+   $list.on('click', 'li', function () {
+    $().addClass('li');
+    
+   });
+    //naming convention 
+    $li.append($writtenDl);
+    $list.prepend($li);
+    $name.val('');
 });
-
 
